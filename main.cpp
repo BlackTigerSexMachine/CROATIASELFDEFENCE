65961f5c038e8a4ba2377810e45e61747c619d10
Winny <olsvoron@gmail.com>
2019-03-25 09:10:03 +0200

    kosar za penek

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 using namespace std;
+ class A{
+ private:
+     int i;
+ public:
+     A(int ii);
+     A(const A & a);
+     A& operator= (const A& a);
+     void show() {std::cout<<i<<std::endl;}
+ };
 
+ A::A(int ii){
+     i=ii;
+ }
+
+ A::A(const A & a){
+     i=a.i;
+     i++;
+ }
+
+ A& A::operator= (const A& a)
+ {
+     if (this==&A) return *this;
+     i=a.i;
+     i--;
+     return *this;
+ }
 int main()
 {
-    cout << "Git hello \n";
-//zalupa
-    return 0;
+   A a(4);
+   A b=a;
+   b.show();
+   return 0;
 }
